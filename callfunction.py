6ed599310691061fec0d66018791eef3461156d5
ikiliagwuuso <72145967+ikiliagwuuso@users.noreply.github.com>
2020-11-29 07:28:52 -0500

    Stutern

---
+++ callfunction.py
@@ -0,0 +1,13 @@
+import data_app
+
+#data_app.add_one('Nonso','I','nonnyi@gmail.com','DataScience')
+#data_app.delete_one(6)
+#data_app.delete_one('12')
+
+# stuff = [('Ade', 'Ciroma', 'c.ade@gmail.com', 'DataScience'),
+#         ('Olamide', 'Ogundele', 'oo@gmail.com', 'DataScience')
+#         ]
+
+#data_app.add_many(stuff)
+#data_app.email_lookup('uso@yahoo.com')
+#data_app.show_all()
